449d3cf843306e70a733709cb2ca3fe154d90e9c
pavlo-zinchenko <pavlozinchenko.pz@gmail.com>
2025-07-26 10:36:28 +0200

    fix: remove is weekend check

--- src/jobs/agenda.service.ts
+++ src/jobs/agenda.service.ts
@@ -56,59 +56,42 @@ export class AgendaService implements OnModuleInit {
 
       for (const task of activeTasks) {
         if (this.isTaskTimeToRun(task)) {
-          this.taskProcessorService
-            .processTasks(task._id.toString())
-            .catch((e) => {
-              this.logger.error(
-                `Error processing task ${task._id}: ${e.message}`,
-              );
-            });
+          this.taskProcessorService.processTasks(task._id.toString()).catch((e) => {
+            this.logger.error(`Error processing task ${task._id}: ${e.message}`);
+          });
         } else {
           this.logger.debug(`Task ${task._id} is not scheduled to run now`);
         }
       }
     } catch (error) {
-      this.logger.error(
-        `Error processing active tasks: ${error.message}`,
-        error,
-      );
+      this.logger.error(`Error processing active tasks: ${error.message}`, error);
     }
   }
 
   private isTaskTimeToRun(task: Task): boolean {
     const now = new Date();
-    const dayOfWeek = now.getDay();
-    const isSaturday = dayOfWeek === 6;
-    const isSunday = dayOfWeek === 0;
-    const isWeekday = !isSaturday && !isSunday;
 
-    if (!isWeekday) {
-      this.logger.info('Not a weekday, skipping task');
-      return false;
-    }
     const currentTime = now.getHours() * 60 + now.getMinutes();
     const [fromHour, fromMin] = task.timeFrom.split(':').map(Number);
     const [toHour, toMin] = task.timeTo.split(':').map(Number);
     const timeFrom = fromHour * 60 + fromMin;
     const timeTo = toHour * 60 + toMin;
-    this.logger.debug(
-      `Checking task time: current=${currentTime}, from=${timeFrom}, to=${timeTo}`,
-    );
+    this.logger.debug(`Checking task time: current=${currentTime}, from=${timeFrom}, to=${timeTo}`);
     let inTime = false;
+
     if (timeFrom < timeTo) {
       inTime = currentTime >= timeFrom && currentTime <= timeTo;
     } else if (timeFrom > timeTo) {
       inTime = currentTime >= timeFrom || currentTime <= timeTo;
     }
+
     if (!inTime) return false;
 
     if (task.lastRunAt) {
       const lastRun = new Date(task.lastRunAt).getTime();
       const intervalMs = (task.intervalMinutes || 1) * 60 * 1000;
       if (now.getTime() - lastRun < intervalMs) {
-        this.logger.debug(
-          `Task interval not reached: lastRunAt=${task.lastRunAt}`,
-        );
+        this.logger.debug(`Task interval not reached: lastRunAt=${task.lastRunAt}`);
         return false;
       }
     }
@@ -118,24 +101,16 @@ export class AgendaService implements OnModuleInit {
   private async scheduleJob(cronExpression: string, jobName: string) {
     await agenda.every(cronExpression, jobName);
     const jobs = await agenda.jobs({ name: jobName });
-    const nextRunAt =
-      jobs.length > 0 ? jobs[0].attrs.nextRunAt : 'NOT SCHEDULED!!!!!';
-    this.logger.info(
-      `Scheduled "${jobName}" job: "${cronExpression}". Next run at: ${nextRunAt}`,
-    );
+    const nextRunAt = jobs.length > 0 ? jobs[0].attrs.nextRunAt : 'NOT SCHEDULED!!!!!';
+    this.logger.info(`Scheduled "${jobName}" job: "${cronExpression}". Next run at: ${nextRunAt}`);
   }
 
   private async resetAllTaskLocks(): Promise<void> {
     try {
-      const result = await this.taskModel.updateMany(
-        { isRunning: true },
-        { isRunning: false },
-      );
+      const result = await this.taskModel.updateMany({ isRunning: true }, { isRunning: false });
 
       if (result.modifiedCount > 0) {
-        this.logger.info(
-          `Reset ${result.modifiedCount} task locks on server startup`,
-        );
+        this.logger.info(`Reset ${result.modifiedCount} task locks on server startup`);
       } else {
         this.logger.info('No task locks found to reset');
       }
